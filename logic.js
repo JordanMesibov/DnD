83ac3e3c6677e0b9532ca48fdb54bd96893640ee
Jordan Mesibov <mesibovj@gmail.com>
2019-08-10 01:17:41 -0400

    set up the thaum function to overwrite the html of the overwrite div and then to append the rest of the relevant thaum info below it

---
+++ logic.js
@@ -0,0 +1,10 @@
+function thaum() {
+    $("#overwrite").html("<p>Thaumaturgy</p>");
+    $("#overwrite").append("<p>You manifest a minor wonder, a sign of supernatural power, within range (30 ft). You create one of the following magical effects within range:</p>");
+    $("#overwrite").append("<p>• Your voice booms up to three times as loud as normal for 1 minute.</p>");
+    $("#overwrite").append("<p>• You cause flames to flicker, brighten, dim, or change color for 1 minute.</p>");
+    $("#overwrite").append("<p>• You cause harmless tremors in the ground for 1 minute.</p>");
+    $("#overwrite").append("<p>• You create an instantaneous sound that originates from a point of your choice within range, such as a rumble of thunder, the cry of a raven, or ominous whispers.</p>");
+    $("#overwrite").append("<p>• You instantaneously cause an unlocked door or window to fly open or slam shut. • You alter the appearance of your eyes for 1 minute. If you cast this spell multiple times, you can have up to three of its 1-minute effects active at a time, and you can dismiss such an effect as an action.</p>");
+}
+
