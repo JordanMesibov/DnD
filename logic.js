17fd0bf957abd6d03b1c90daf8636e4e1766f877
Jordan Mesibov <mesibovj@gmail.com>
2019-08-19 21:11:06 -0400

    added proper hexblade spells and removed the other lv 1 spells, and added the proper javascript functionality to update the page accordingly

--- logic.js
+++ logic.js
@@ -11,6 +11,21 @@ function thaum() {
     $("#overwrite").append("<p>• You alter the appearance of your eyes for 1 minute. If you cast this spell multiple times, you can have up to three of its 1-minute effects active at a time, and you can dismiss such an effect as an action.</p>");
 }
 
+function hexbladesCurse() {
+    $("#overwrite").html("<p>Hexblade's Curse</p>");
+    $("#overwrite").append("<p>Starting at 1st level, you gain the ability to place a baleful curse on someone. As a bonus action, choose one creature you can see within 30 feet of you. The target is cursed for 1 minute. The curse ends early if the target dies, you die, or you are incapacitated. Until the curse ends, you gain the following benefits:</p>");
+    $("#overwrite").append("<p>• You gain a bonus to damage rolls against the cursed target. The bonus equals your proficiency bonus.</p>");
+    $("#overwrite").append("<p>• Any attack roll you make against the cursed target is a critical hit on a roll of 19 or 20 on the d20.</p>");
+    $("#overwrite").append("<p>• If the cursed target dies, you regain hit points equal to your warlock level + your Charisma modifier (minimum of 1 hit point).</p>");
+    $("#overwrite").append("<p>You can’t use this feature again until you finish a short or long rest.</p>");
+}
+
+function hexWarrior() {
+    $("#overwrite").html("<p>Hex Warrior</p>");
+    $("#overwrite").append("<p>At 1st level, you acquire the training necessary to effectively arm yourself for battle. You gain proficiency with medium armor, shields, and martial weapons.</p>");
+    $("#overwrite").append("<p>The influence of your patron also allows you to mystically channel your will through a particular weapon. Whenever you finish a long rest, you can touch one weapon that you are proficient with and that lacks the two-handed property. When you attack with that weapon, you can use your Charisma modifier, instead of Strength or Dexterity, for the attack and damage rolls. This benefit lasts until you finish a long rest. If you later gain the Pact of the Blade feature, this benefit extends to every pact weapon you conjure with that feature, no matter the weapon's type.</p>");
+}
+
 function burning() {
     $("#overwrite").html("<p>Burning Hands</p>");
     $("#overwrite").append("<p>As you hold your hands with thumbs touching and fingers spread, a thin sheet of flames shoots forth from your outstretched fingertips. Each creature in a 15 ft cone must make a dexterity saving throw. A creature takes 3d6 fire damage on a failed save, or half as much damage on a successful one. The fire ignites any flammable objects in the area that aren't being worn or carried. At Higher Levels. When you cast this spell using a spell slot of 2nd level or higher, the damage increases by 1d6 for each slot level above 1st.</p>");
@@ -44,6 +59,19 @@ function hellish() {
 function infernal() {
     $("#overwrite").html("<p>Infernal Legacy</p>");
     $("#overwrite").append("<p>You know thaumaturgy and can cast  once per day. CHA is the spellcasting ability.</p>");
+}
+
+function shield() {
+    $("#overwrite").html("<p>Shield</p>");
+    $("#overwrite").append("<p>Casting Time: 1 reaction, which you take when you are hit by an attack or targeted by the Magic Missile spell</p>");
+    $("#overwrite").append("<p>An invisible barrier of magical force appears and protects you. Until the start of your next turn, you have a +5 bonus to Armor Class, including against the triggering attack, and you take no damage from Magic Missile.</p>");
+}
+
+function wrathfulSmight() {
+    $("#overwrite").html("<p>Wrathful Smight</p>");
+    $("#overwrite").append("<p>Casting Time: 1 bonus action</p>");
+    $("#overwrite").append("<p>Duration: Concentration, up to 1 minute</p>");
+    $("#overwrite").append("<p>The next time you hit with a melee weapon attack during this spell’s duration, your attack deals an extra 1d6 psychic damage. Additionally, if the target is a creature, it must make a Wisdom saving throw or be frightened of you until the spell ends. As an action, the creature can make a Wisdom check against your spell save DC to steel its resolve and end this spell.</p>");
 }
 
 function quarterstaff() {
